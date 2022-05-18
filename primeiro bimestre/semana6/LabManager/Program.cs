dfc9f488acf7ac8109a6d7847615deb99084a3f5
Alexsandro de Souza Campos <97525102+AlexsandroCampos@users.noreply.github.com>
2022-05-18 11:55:17 -0300

    Atividades realizadas durante a semana 7

--- primeiro bimestre/semana6/LabManager/Program.cs
+++ primeiro bimestre/semana6/LabManager/Program.cs
@@ -1,33 +1,10 @@
 using Microsoft.Data.Sqlite;
+using LabManager.Database;
+using LabManager.Repositories;
 
-var connection = new SqliteConnection("Data Source=database.db");
-connection.Open();
-var command = connection.CreateCommand();
-command.CommandText = @"
-CREATE TABLE IF NOT EXISTS Computers(
-    id int not null primary key,
-    ram varchar(100) not null,
-    processor varchar(100) not null
-);
-";
-command.ExecuteNonQuery();
-connection.Close();
+var databaseSetup = new DatabaseSetup();
 
-
-
-connection = new SqliteConnection("Data Source=database.db");
-connection.Open();
-command = connection.CreateCommand();
-command.CommandText = @"
-CREATE TABLE IF NOT EXISTS Lab(
-    id int not null primary key,
-    number int not null,
-    name varchar(100) not null,
-    block varchar(50) not null
-);
-";
-command.ExecuteNonQuery();
-connection.Close();
+var computerRepository = new ComputerRepository();
 
 // Routing
 var modelName = args[0];
@@ -38,22 +15,10 @@ if (modelName == "Computer")
 {
     if (modelAction == "List")
     {
-            connection = new SqliteConnection("Data Source=database.db");
-            connection.Open();
-            command = connection.CreateCommand();
-            command.CommandText = "SELECT * FROM Computers;";
-
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.WriteLine(
-                    "{0}, {1}, {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2)
-                    );
-            }
-
-            reader.Close();
-            connection.Close(); 
+           foreach (var computer in computerRepository.GetAll())
+           {
+               Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
+           }
 
     }
     if (modelAction == "New")
@@ -62,10 +27,10 @@ if (modelName == "Computer")
             string ram = args[3];
             string processor = args[4];
 
-            connection = new SqliteConnection("Data Source=database.db");
+            var connection = new SqliteConnection("Data Source=database.db");
             connection.Open();
 
-            command = connection.CreateCommand();
+            var command = connection.CreateCommand();
             command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor);";
             command.Parameters.AddWithValue("$id", id);
             command.Parameters.AddWithValue("$ram", ram);
@@ -80,10 +45,10 @@ if (modelName == "Lab")
 {
     if (modelAction == "List")
     {
-        connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
 
-        command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = "SELECT * FROM Lab;";
 
          var reader = command.ExecuteReader();
@@ -104,10 +69,10 @@ if (modelName == "Lab")
         string block = args[5];
 
         
-        connection = new SqliteConnection("Data Source=database.db");
+        var connection = new SqliteConnection("Data Source=database.db");
         connection.Open();
 
-        command = connection.CreateCommand();
+        var command = connection.CreateCommand();
         command.CommandText = "INSERT INTO Lab VALUES($id, $number, $name, $block);";
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$number", number);
